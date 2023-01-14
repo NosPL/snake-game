f70d48bbae536470ba59aa3c9524e6aa7c76af66
nos <nos217@o2.pl>
2023-01-14 15:54:31 +0100

    remote server module change:
    host name is now a static string "snake-online.herokuapp.com"
    
    port is provided from enviroment variables or its 8080 in error occurs
    
    added Procfile and system.properties for heroku deployment

--- snake-game-online-remote-server/src/main/java/com/noscompany/snake/game/online/remote/server/LogHostEventHandler.java
+++ snake-game-online-remote-server/src/main/java/com/noscompany/snake/game/online/remote/server/LogHostEventHandler.java
@@ -8,101 +8,102 @@ import com.noscompany.snake.game.online.contract.messages.room.FailedToEnterRoom
 import com.noscompany.snake.game.online.contract.messages.room.NewUserEnteredRoom;
 import com.noscompany.snake.game.online.contract.messages.room.UserLeftRoom;
 import com.noscompany.snake.game.online.host.room.mediator.ports.RoomEventHandlerForHost;
+import lombok.extern.slf4j.Slf4j;
 
-class NullHostEventHandler implements RoomEventHandlerForHost {
+@Slf4j
+class LogHostEventHandler implements RoomEventHandlerForHost {
 
     @Override
     public void handle(FailedToEnterRoom failedToEnterRoom) {
-
+        log.info("{}", failedToEnterRoom);
     }
 
     @Override
     public void handle(UserSentChatMessage userSentChatMessage) {
-
+        log.info("{}", userSentChatMessage);
     }
 
     @Override
     public void handle(GameOptionsChanged gameOptionsChanged) {
-
+        log.info("{}", gameOptionsChanged);
     }
 
     @Override
     public void handle(FailedToSendChatMessage event) {
-
+        log.info("{}", event);
     }
 
     @Override
     public void handle(FailedToStartGame event) {
-
+        log.info("{}", event);
     }
 
     @Override
     public void handle(FailedToChangeGameOptions event) {
-
+        log.info("{}", event);
     }
 
     @Override
     public void handle(PlayerFreedUpASeat event) {
-
+        log.info("{}", event);
     }
 
     @Override
     public void handle(FailedToFreeUpSeat event) {
-
+        log.info("{}", event);
     }
 
     @Override
     public void handle(PlayerTookASeat event) {
-
+        log.info("{}", event);
     }
 
     @Override
     public void handle(FailedToTakeASeat event) {
-
+        log.info("{}", event);
     }
 
     @Override
     public void handle(TimeLeftToGameStartHasChanged event) {
-
+        log.info("{}", event);
     }
 
     @Override
     public void handle(GameStarted event) {
-
+        log.info("{}", event);
     }
 
     @Override
     public void handle(GameContinues event) {
-
     }
 
     @Override
     public void handle(GameFinished event) {
-
+        log.info("{}", event);
     }
 
     @Override
     public void handle(GameCancelled event) {
-
+        log.info("{}", event);
     }
 
     @Override
     public void handle(GamePaused event) {
-
+        log.info("{}", event);
     }
 
     @Override
     public void handle(GameResumed event) {
-
+        log.info("{}", event);
     }
 
     @Override
     public void handle(NewUserEnteredRoom event) {
-
+        log.info("{}", event);
     }
 
     @Override
     public void handle(UserLeftRoom event) {
-
+        log.info("{}", event);
     }
 }
--- snake-game-online-remote-server/src/main/java/com/noscompany/snake/game/online/remote/server/Main.java
+++ snake-game-online-remote-server/src/main/java/com/noscompany/snake/game/online/remote/server/Main.java
@@ -3,16 +3,28 @@ package com.noscompany.snake.game.online.remote.server;
 import com.noscompany.snake.game.online.host.room.mediator.RoomMediator;
 import com.noscompany.snake.game.online.host.room.mediator.RoomMediatorConfiguration;
 import com.noscompany.snake.game.online.host.server.Server;
+import com.noscompany.snake.game.online.host.server.dto.ServerParams;
 import com.noscompany.snake.game.online.host.server.nettosphere.ServerConfiguration;
 
-import static com.noscompany.snake.game.online.remote.server.ServerParamsCreator.toServerParams;
+import static io.vavr.API.Try;
 
 class Main {
+    private static final String HOST = "snake-online.herokuapp.com";
+    private static final int DEFAULT_PORT = 8080;
 
     public static void main(String[] args) {
         Server server = new ServerConfiguration().createServer();
-        RoomMediator roomMediator = new RoomMediatorConfiguration().roomMediator(new NullHostEventHandler(), server);
-        server.start(toServerParams(args), roomMediator);
+        RoomMediator roomMediator = new RoomMediatorConfiguration().roomMediator(new LogHostEventHandler(), server);
+        server.start(serverParams(), roomMediator);
     }
 
+    private static ServerParams serverParams() {
+        return new ServerParams(HOST, getPort());
+    }
+
+    private static Integer getPort() {
+        return Try(() -> System.getenv("PORT"))
+                .map(Integer::parseInt)
+                .getOrElse(DEFAULT_PORT);
+    }
 }
