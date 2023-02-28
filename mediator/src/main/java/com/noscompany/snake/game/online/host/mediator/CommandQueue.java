0262defdfa433dde5dd56c37f9c758700b72ad19
nos <nos217@o2.pl>
2023-02-28 21:46:00 +0100

    Added logs to mediator module

--- mediator/src/main/java/com/noscompany/snake/game/online/host/mediator/CommandQueue.java
+++ mediator/src/main/java/com/noscompany/snake/game/online/host/mediator/CommandQueue.java
@@ -6,9 +6,11 @@ import com.noscompany.snake.game.online.contract.messages.gameplay.dto.PlayerNum
 import com.noscompany.snake.game.online.contract.messages.room.UserName;
 import com.noscompany.snake.game.online.host.server.dto.RemoteClientId;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.ExecutorService;
 
+@Slf4j
 @AllArgsConstructor
 class CommandQueue implements Mediator {
     private final ExecutorService executor;
@@ -16,117 +18,140 @@ class CommandQueue implements Mediator {
 
     @Override
     public void enter(HostId hostId, UserName userName) {
+        log.info("host puts 'enter room' command in queue");
         executor.submit(() -> commandHandler.enter(hostId, userName));
     }
 
     @Override
     public void sendChatMessage(HostId hostId, String messageContent) {
+        log.info("host puts 'send chat message' command in queue");
         executor.submit(() -> commandHandler.sendChatMessage(hostId, messageContent));
     }
 
     @Override
     public void cancelGame(HostId hostId) {
+        log.info("host puts 'cancel game' command in queue");
         executor.submit(() -> commandHandler.cancelGame(hostId));
     }
 
     @Override
     public void changeSnakeDirection(HostId hostId, Direction direction) {
+        log.debug("host puts 'change snake direction' command in queue");
         executor.submit(() -> commandHandler.changeSnakeDirection(hostId, direction));
     }
 
     @Override
     public void pauseGame(HostId hostId) {
+        log.info("host puts 'pause game' command in queue");
         executor.submit(() -> commandHandler.pauseGame(hostId));
     }
 
     @Override
     public void resumeGame(HostId hostId) {
+        log.info("host puts 'resume game' command in queue");
         executor.submit(() -> commandHandler.resumeGame(hostId));
     }
 
     @Override
     public void startGame(HostId hostId) {
+        log.info("host puts 'start game' command in queue");
         executor.submit(() -> commandHandler.startGame(hostId));
     }
 
     @Override
     public void changeGameOptions(HostId hostId, GameOptions gameOptions) {
+        log.info("host puts 'change game options' command in queue");
         executor.submit(() -> commandHandler.changeGameOptions(hostId, gameOptions));
     }
 
     @Override
     public void freeUpASeat(HostId hostId) {
+        log.info("host puts 'free up a seat' command in queue");
         executor.submit(() -> commandHandler.freeUpASeat(hostId));
     }
 
     @Override
     public void takeASeat(HostId hostId, PlayerNumber playerNumber) {
+        log.info("host puts 'take a seat' command in queue");
         executor.submit(() -> commandHandler.takeASeat(hostId, playerNumber));
     }
 
     @Override
     public void shutdown() {
+        log.info("shutting mediator command queue down");
         executor.shutdown();
         commandHandler.shutdown();
     }
 
     @Override
     public void sendChatMessage(RemoteClientId remoteClientId, String messageContent) {
+        log.info("remote client with id {} puts 'send chat message' command in queue", remoteClientId.getId());
         executor.submit(() -> commandHandler.sendChatMessage(remoteClientId, messageContent));
     }
 
     @Override
     public void cancelGame(RemoteClientId remoteClientId) {
+        log.info("remote client with id {} puts 'cancel game' command in queue", remoteClientId.getId());
         executor.submit(() -> commandHandler.cancelGame(remoteClientId));
     }
 
     @Override
     public void changeDirection(RemoteClientId remoteClientId, Direction direction) {
+        log.debug("remote client with id {} puts 'change snake direction' command in queue", remoteClientId.getId());
         executor.submit(() -> commandHandler.changeDirection(remoteClientId, direction));
     }
 
     @Override
     public void pauseGame(RemoteClientId remoteClientId) {
+        log.info("remote client with id {} puts 'pause game' command in queue", remoteClientId.getId());
         executor.submit(() -> commandHandler.pauseGame(remoteClientId));
     }
 
     @Override
     public void resumeGame(RemoteClientId remoteClientId) {
+        log.info("remote client with id {} puts 'resume game' command in queue", remoteClientId.getId());
         executor.submit(() -> commandHandler.resumeGame(remoteClientId));
     }
 
     @Override
     public void startGame(RemoteClientId remoteClientId) {
+        log.info("remote client with id {} puts 'start game' command in queue", remoteClientId.getId());
         executor.submit(() -> commandHandler.startGame(remoteClientId));
     }
 
     @Override
     public void changeGameOptions(RemoteClientId remoteClientId, GameOptions gameOptions) {
+        log.info("remote client with id {} puts 'change game options' command in queue", remoteClientId.getId());
         executor.submit(() -> commandHandler.changeGameOptions(remoteClientId, gameOptions));
     }
 
     @Override
     public void freeUpSeat(RemoteClientId remoteClientId) {
+        log.info("remote client with id {} puts 'free up a seat' command in queue", remoteClientId.getId());
         executor.submit(() -> commandHandler.freeUpSeat(remoteClientId));
     }
 
     @Override
     public void takeASeat(RemoteClientId remoteClientId, PlayerNumber playerNumber) {
+        log.info("remote client with id {} puts 'take a seat' command in queue", remoteClientId.getId());
         executor.submit(() -> commandHandler.takeASeat(remoteClientId, playerNumber));
     }
 
     @Override
     public void enterRoom(RemoteClientId remoteClientId, UserName userName) {
+        log.info("remote client with id {} puts 'enter room' command in queue", remoteClientId.getId());
         executor.submit(() -> commandHandler.enterRoom(remoteClientId, userName));
     }
 
     @Override
     public void leaveRoom(RemoteClientId remoteClientId) {
+        log.info("remote client with id {} puts 'leave room' command in queue", remoteClientId.getId());
         executor.submit(() -> commandHandler.leaveRoom(remoteClientId));
     }
 
     @Override
     public void initializeClientState(RemoteClientId remoteClientId) {
+        log.info("remote client with id {} puts 'initialize client state' command in queue", remoteClientId.getId());
         executor.submit(() -> commandHandler.initializeClientState(remoteClientId));
     }
 }
--- mediator/src/main/java/com/noscompany/snake/game/online/host/mediator/EventDispatcher.java
+++ mediator/src/main/java/com/noscompany/snake/game/online/host/mediator/EventDispatcher.java
@@ -17,110 +17,138 @@ import com.noscompany.snake.game.online.host.RoomEventHandlerForHost;
 import com.noscompany.snake.game.online.host.server.Server;
 import com.noscompany.snake.game.online.host.server.dto.RemoteClientId;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import snake.game.gameplay.GameplayEventHandler;
 
+import java.util.Locale;
+
 @AllArgsConstructor
+@Slf4j
 class EventDispatcher implements GameplayEventHandler {
     private final RoomEventHandlerForHost host;
     private final Server server;
 
     void sendToClientsAndHost(UserSentChatMessage event) {
+        log.info("passing 'user sent chat message' event to server and host");
         server.sendToAllClients(event);
         host.handle(event);
     }
 
     void sendToHost(FailedToSendChatMessage event) {
+        log.info("passing 'failed to send chat message' event to host");
         host.handle(event);
     }
 
     void sendToHost(FailedToStartGame event) {
+        log.info("passing 'failed to start game' event to host");
         host.handle(event);
     }
 
     void sendToClientsAndHost(GameOptionsChanged event) {
+        log.info("passing 'game options changed' event to server and host");
         server.sendToAllClients(event);
         host.handle(event);
     }
 
     void sendToHost(FailedToChangeGameOptions event) {
+        log.info("passing 'failed to change game options' event to host");
         host.handle(event);
     }
 
     void sendToClientsAndHost(PlayerFreedUpASeat event) {
+        log.info("passing 'player freed up a seat' event to server and host");
         server.sendToAllClients(event);
         host.handle(event);
     }
 
     void sendToHost(FailedToFreeUpSeat event) {
+        log.info("passing 'failed to free up a seat' event to host");
         host.handle(event);
     }
 
     void sendToClientsAndHost(PlayerTookASeat event) {
+        log.info("passing 'player took a seat' event to server and host");
         server.sendToAllClients(event);
         host.handle(event);
     }
 
     void sendToClientsAndHost(NewUserEnteredRoom event) {
+        log.info("passing 'new user entered room' event to server and host");
         server.sendToAllClients(event);
         host.handle(event);
     }
 
     void sendToHost(FailedToTakeASeat event) {
+        log.info("passing 'failed to take a seat' event to host");
         host.handle(event);
     }
 
     void sendToClient(RemoteClientId remoteClientId, OnlineMessage onlineMessage) {
+        log.info("passing '{}' message to server for remote client with id {}", asString(onlineMessage.getMessageType()), remoteClientId.getId());
         server.sendToClientWithId(remoteClientId, onlineMessage);
     }
 
     void sendToClientsAndHost(UserLeftRoom event) {
+        log.info("passing 'user left room' event to server and host");
         server.sendToAllClients(event);
         host.handle(event);
     }
 
     void sendToHost(FailedToEnterRoom failedToEnterRoom) {
+        log.info("passing 'failed to enter room' event to host");
         host.handle(failedToEnterRoom);
     }
 
     @Override
     public void handle(TimeLeftToGameStartHasChanged event) {
+        log.info("passing 'time left to game start has changed' event to server and host");
         server.sendToAllClients(event);
         host.handle(event);
     }
 
     @Override
     public void handle(GameStarted event) {
+        log.info("passing 'game started' event to server and host");
         server.sendToAllClients(event);
         host.handle(event);
     }
 
     @Override
     public void handle(SnakesMoved event) {
+        log.info("passing 'snakes moved' event to server and host");
         server.sendToAllClients(event);
         host.handle(event);
     }
 
     @Override
     public void handle(GameFinished event) {
+        log.info("passing 'game finished' event to server and host");
         server.sendToAllClients(event);
         host.handle(event);
     }
 
     @Override
     public void handle(GameCancelled event) {
+        log.info("passing 'game cancelled' event to server and host");
         server.sendToAllClients(event);
         host.handle(event);
     }
 
     @Override
     public void handle(GamePaused event) {
+        log.info("passing 'game paused' event to server and host");
         server.sendToAllClients(event);
         host.handle(event);
     }
 
     @Override
     public void handle(GameResumed event) {
+        log.info("passing 'game resumed' event to server and host");
         server.sendToAllClients(event);
         host.handle(event);
+    }
+
+    private String asString(OnlineMessage.MessageType messageType) {
+        return messageType.toString().toLowerCase(Locale.ROOT).replace('_', ' ');
     }
 }
