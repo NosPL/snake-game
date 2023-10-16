9ae68c9142449243ccac31e076f52eaa0bbdde92
nos <nos217@o2.pl>
2023-10-16 22:51:20 +0200

    - extracted user-registry-gui to separate module

--- users-registry-gui/src/main/java/com/noscompany/snake/game/online/user/registry/gui/UsersRegistryController.java
+++ users-registry-gui/src/main/java/com/noscompany/snake/game/online/user/registry/gui/UsersRegistryController.java
@@ -1,4 +1,4 @@
-package com.noscompany.snakejavafxclient.components.online.game.commons;
+package com.noscompany.snake.game.online.user.registry.gui;
 
 import com.noscompany.message.publisher.Subscription;
 import com.noscompany.snake.game.online.contract.messages.user.registry.NewUserEnteredRoom;
@@ -10,7 +10,7 @@ import javafx.scene.control.Label;
 
 import java.util.Collection;
 
-public class JoinedUsersController extends AbstractController {
+public class UsersRegistryController extends AbstractController {
     @FXML
     private Label usersListLabel;
 
