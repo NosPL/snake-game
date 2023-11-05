97e45bde336498da7499a0527187eda5fec831b1
nos <nos217@o2.pl>
2023-11-05 12:40:42 +0100

    Gui changes: - removed TestLauncher from main-launcher module (test has its own separate launcher) - removed ApplicationProfile.java (not used)

--- gui-commons/src/main/java/com/noscompany/snake/game/online/gui/commons/ApplicationProfile.java
+++
@@ -1,8 +0,0 @@
-package com.noscompany.snake.game.online.gui.commons;
-
-public class ApplicationProfile {
-    public enum Profile {
-        PROD, TEST
-    }
-    public static Profile profile = Profile.PROD;
-}
--- main-launcher/src/main/java/com/noscompany/snakejavafxclient/Launcher.java
+++ main-launcher/src/main/java/com/noscompany/snakejavafxclient/Launcher.java
@@ -1,11 +1,8 @@
 package com.noscompany.snakejavafxclient;
 
-import com.noscompany.snake.game.online.gui.commons.ApplicationProfile;
-
 public class Launcher {
 
     public static void main(String[] args) {
-        ApplicationProfile.profile = ApplicationProfile.Profile.PROD;
         SnakeGameApplication.main(args);
     }
 }
